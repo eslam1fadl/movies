1eaee430792cef40f4d5879a85cb2217f03e330f
eslam1fadl <eslam110@stud.fci-cu.edu.eg>
2025-06-07 15:49:39 +0300

    Initial commit

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Spinner() {
   return (
     <div role="status">
-     <svg className="animate-spin h-8 w-8 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+     {/* <svg className="animate-spin h-8 w-8 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z"></path>
-</svg>
+</svg> */}
 
       <span className="sr-only">Loading...</span>
     </div>
